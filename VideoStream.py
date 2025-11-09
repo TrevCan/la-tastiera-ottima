7bee29318fca35d57d3b67aed6fbd148cadb3238
human <email>
2025-11-09 16:41:03 -0600

    Change VideoStream.py to Singleton class, much more manageable when making calls in mouseCallback functions.

--- VideoStream.py
+++ VideoStream.py
@@ -2,8 +2,9 @@ import cv2
 import threading
 from threading import Thread
 
+from Singleton import Singleton
 
-class VideoStream:
+class VideoStream (metaclass=Singleton):
     """
     Class for grabbing frames from CV2 video capture in a dedicated thread.
     Generate a new object with VideoStream.VideoStream(src)
@@ -29,7 +30,15 @@ class VideoStream:
         else:
             (self.grabbed, self.frame) = self.stream.read()
             
-        self.stopped = False
+        ### --Start-- Thread operation flags
+        ## Flags for
+        ## RESUME, PAUSE, and STOP
+        self.__go_flag = threading.Event() # Create new flag 'Event'
+        self.__go_flag.set() # set flag to True
+
+        self.__is_running = threading.Event()
+        self.__is_running.set()
+        ### --End-- Thread operation flags
 
     def start(self):
         """
@@ -39,19 +48,7 @@ class VideoStream:
             Thread(target=self.get, args=()).start()
         return self
 
-    def get(self):
-        """
-        Continuously gets frames from CV2 VideoCapture and sets them as self.frame attribute
-        """
-        while not self.stopped:
-            # We explicitly check if the stream is still open
-            if self.stream.isOpened():
-                (self.grabbed, self.frame) = self.stream.read()
-                #print(f'Grab frame: {self.grabbed}')
-            else:
-                self.frame = None
-                self.stopped = True # Stop the thread if stream is no longer open
-            
+           
     def get_video_dimensions(self):
         """
         Gets the width and height of the video stream frames
@@ -62,29 +59,81 @@ class VideoStream:
             return int(width), int(height)
         return 0, 0
 
-    def stop_process(self):
+    def shape(self):
         """
-        Sets the self.stopped attribute as True and kills the VideoCapture stream read
+        Get frame shape in (y, x) format.
+        cv2 likes it like that!!!
         """
-        self.stopped = True
+        x, y = self.get_video_dimensions()
+        return (y, x)
+
+    def release(self):
+        """
+        Calls self.stop()
+        """
+        self.stop()
+    
+    def is_running(self):
+        """
+        Returns whether the thread is running or not.
+        It will return True if the thread is running.
+        It will return False if it is paused or permanently
+            stopped or exited.
+        """
+        return self.__is_running.isSet() & self.__go_flag.isSet()
+
+    def pause(self):
+        """
+        If running, the thread will pause operatoins.
+        If stop() or exit() have been called, the
+        thread cannot be resumed or paused.
+        """
+        self.__go_flag.clear() # Sets __go_flag to FALSE
+
+    def resume(self):
+        """
+        If paused, the thread will resume operations.
+        If stop() or exit() have been called, the
+        thread cannot be resumed or paused.
+        """
+        self.__go_flag.set() # Sets __go_flag to True
+
+    def stop(self):
+        """
+        Permanently stops thread, then runs self.exit_gracefully() from the thread.
+        """
+        self.resume()
+        self.__is_running.clear()
+    
+    def exit(self):
+        """
+        Calls self.stop()
+        """
+        self.stop()
+
+    def exit_gracefully(self):
         if self.stream.isOpened():
             self.stream.release()
             print(f"VideoStream {self.stream} successfully closed.")
 
-    def release(self):
-        """
-        Calls self.stop_process()
-        """
-        self.stop_process()
-    
-    def close(self):
-        """
-        Calls self.stop_process()
-        """
-        self.stop_process()
+        print(f'\nGoodbye from {self.__class__.__name__}!')
 
-    def exit(self):
+    def get(self):
         """
-        Calls self.stop_process()
+        Continuously gets frames from CV2 VideoCapture and sets them as self.frame attribute
         """
-        self.stop_process()
+        while self.__is_running.isSet():
+            self.__go_flag.wait() # If __go_flag is False it will not run
+                                    # until set to True, therefore 'pausing'
+                                    # the thread.
+                
+            # We explicitly check if the stream is still open
+            if self.stream.isOpened():
+                (self.grabbed, self.frame) = self.stream.read()
+                #print(f'Grab frame: {self.grabbed}')
+            else:
+                self.frame = None
+        
+        # After closing, run exit_gracefully
+        self.exit_gracefully()
+ 
